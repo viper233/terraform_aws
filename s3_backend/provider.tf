735be19ea46af08482eefcd9a97b2a87048566bb
Stephen Granger <viper233@gmail.com>
2024-02-07 15:56:36 -0800

    Set verions, provider configurations

--- s3_backend/provider.tf
+++ s3_backend/provider.tf
@@ -1,3 +1,14 @@
+terraform {
+  required_version = "~> 1.6.1"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.35.0"
+    }
+  }
+}
+
 provider "aws" {
   region = "us-west-2"
 }
