93964bb38f0b4413b265cf822e7f60b4c6f27b94
Stephen Granger <viper233@gmail.com>
2024-02-07 16:51:16 -0800

    Remove duplicate provider config

--- backend.tf
+++ backend.tf
@@ -5,15 +5,16 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = ">= 4.67.0"
+      version = ">= 5.35.0"
     }
   }
 
-  required_version = ">= 1.4.2"
+  required_version = "~> 1.6"
 }
 
 provider "aws" {
   default_tags {
     tags = local.tags
   }
+  region = var.aws_region
 }
--- variables.tf
+++ variables.tf
@@ -2,3 +2,8 @@ variable "environment" {
   type = string
   description = "The environment the resource is being provisioned to"
 }
+
+variable "aws_region" {
+  type = string
+  description = "AWS Region"
+}
